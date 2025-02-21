a7ab330d18765ab162c0ce375920a585ed77f459
Igor Cattusso <igor_catusso@hotmail.com>
2025-02-21 17:42:36 -0300

    added repositories to injector module

--- src/taco_injector_module.py
+++ src/taco_injector_module.py
@@ -12,13 +12,19 @@ from src.config import Config
 from src.controllers.controller import TestController
 
 # Repositories
+from src.repository.dishes_repository import DishesRepository, DishesRepositoryImpl
+from src.repository.ingredients_repository import IngredientsRepository, IngredientsRepositoryImpl
+from src.repository.measurement_units_repository import MeasurementUnitsRepository, MeasurementUnitsRepositoryImpl
+from src.repository.nutritional_values_repository import NutritionalValuesRepository, NutritionalValuesRepositoryImpl
+from src.repository.preparation_method_repository import PreparationMethodRepository, PreparationMethodRepositoryImpl
 from src.repository.recipes_repository import RecipesRepository, RecipesRepositoryImpl
 
 # Services
+from src.services.utils.utils import Utils, UtilsImpl
 
 # Use Cases
 from src.use_cases.shopping_list_use_cases.generate_shopping_list_use_case import GenerateShoppingListUseCase, \
-    GenerateShoppingListUseCaseImpl
+     GenerateShoppingListUseCaseImpl
 from src.use_cases.recipes_use_cases.print_recipe_use_case import PrintRecipeUseCase, PrintRecipeUseCaseImpl
 
 
@@ -36,7 +42,15 @@ class TacoModule(Module):
         binder.bind(Config, to=Config)
 
         # Database repositories
+        binder.bind(DishesRepository, to=DishesRepositoryImpl)
+        binder.bind(IngredientsRepository, to=IngredientsRepositoryImpl)
+        binder.bind(MeasurementUnitsRepository, to=MeasurementUnitsRepositoryImpl)
+        binder.bind(NutritionalValuesRepository, to=NutritionalValuesRepositoryImpl)
+        binder.bind(PreparationMethodRepository, to=PreparationMethodRepositoryImpl)
         binder.bind(RecipesRepository, to=RecipesRepositoryImpl)
+
+        # Utility services
+        binder.bind(Utils, to=UtilsImpl)
 
         # Use cases
         binder.bind(GenerateShoppingListUseCase, to=GenerateShoppingListUseCaseImpl)
